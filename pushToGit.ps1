8a78452c5e95a7a6c5b2d3305ab6beaaedd030aa
Christof Schwarz <christof.schwarz@databridge.ch>
2022-06-21 12:06:43 +0200

    updates

--- pushToGit.ps1
+++ pushToGit.ps1
@@ -1,9 +1,9 @@
 git add . 
 git commit -m "updates"  
 git push -u origin main
-$confirmation = Read-Host "Increase the version counter now? (y/n)"
+$confirmation = Read-Host "Increase the version counter in .qext file now? (y/n)"
 if ($confirmation -like 'y*') {
-    $qextFilename = '.\ext_guided_tour_2.qext'
+    $qextFilename = Get-ChildItem -Filter '*.qext' | Select-Object -First 1
     $qext = Get-Content $qextFilename -raw | ConvertFrom-Json
     $version = $qext.version.split(".")[2]
     [int]$versionNo = 0
@@ -12,11 +12,11 @@ if ($confirmation -like 'y*') {
         $versionNo = $versionNo + 1
         $versionNoStr = "{0:d$($version.length)}" -f $versionNo
         $versionNoStr = ( -join ($qext.version.split(".")[0], ".", $qext.version.split(".")[1], ".", $versionNoStr))
-        Write-Host "increased version number to $($versionNoStr) for next time"
+        Write-Host "increased version number to $($versionNoStr) in $($qextFilename)"
         $qext.version = $versionNoStr
         $qext | ConvertTo-Json -depth 100 | Out-File $qextFilename -Encoding Utf8
     }
     else {
-        Write-Host "Cannot increase the version number, it isn't"
+        Write-Host "Cannot increase the version number, it isn't an integer"
     }
 }
